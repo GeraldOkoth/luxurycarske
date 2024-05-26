f3a62436da8eaa9ca588d4cbd0a0f19fbfdadbc6
Gerald Okoth <okothgerald449@gmail.com>
2024-05-26 17:36:00 +0300

    Modify code

--- sendEmail.js
+++ sendEmail.js
@@ -9,3 +9,24 @@
     }
     emailjs.send("service_lxmqzq5", "template_6zufajn", params).then(alert("Email has been sent successfully! We will respond to you shortly."))
 }
+
+Script to send email
+	Fom Elasticemail.com/SMTPJS.com
+	This code is not working
+
+    function sendMail(){
+        Email.send({
+			SecureToken: "23a0e1b7-6dd7-4f96-8bc3-3819c13aeb02",
+            To : 'okothgerald449@gmail.com',
+            From : document.getElementById("email").value,
+            Subject : document.getElementById("mesgSubject").value,
+            Body : "First Name: " + document.getElementById("firstname").value +
+                    " <br> Last Name: " + document.getElementById("lastname").value +
+                    " <br> Email: " + document.getElementById("email").value +
+                    " <br> Number: " + document.getElementById("phoneNumber").value +
+                    " <br> Subject: " + document.getElementById("mesgSubject").value +
+                    " <br> Message: " + document.getElementById("msg").value
+        }).then(
+        message => alert("message sent successfully!")
+        );
+    }
